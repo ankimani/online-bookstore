e1e9c950a17e0592cab25a689a9829c1e33cb54c
Albert Njane <anjane@tracom.co.ke>
2020-06-15 15:12:38 +0300

    modified the BookReposirory to allow CORS

--- onlinebookstore/onlinebookstore/src/main/java/albert/com/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/onlinebookstore/src/main/java/albert/com/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,10 @@
 package albert.com.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import albert.com.onlinebookstore.entity.Book;
-
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
