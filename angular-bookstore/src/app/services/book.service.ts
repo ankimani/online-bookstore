aabb435705b5fcddeee224d4e58a69d7020a4efd
Albert Njane <anjane@tracom.co.ke>
2020-07-06 10:12:39 +0300

    finished up bookstore feature 4

--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -3,22 +3,34 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Book } from '../common/Book';
+import { BookCategory } from '../common/book-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
   private baseUrl = "http://localhost:8080/api/v1/books";
+  private categoryUrl = "http://localhost:8080/api/v1/book-category";
   constructor(private httpClient: HttpClient) { }
-  getBooks(theCategoryId :number): Observable<Book[]> {
-    const searchUrl =`${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
+  getBooks(theCategoryId: number): Observable<Book[]> {
+    const searchUrl = `${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
     return this.httpClient.get<getResponseBooks>(searchUrl).pipe(
       map(response => response._embedded.books)
+    )
+  }
+  getbookCategory(): Observable<BookCategory[]> {
+    return this.httpClient.get<getResponseBookCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.bookCategory)
     )
   }
 }
 interface getResponseBooks {
   _embedded: {
     books: Book[];
+  }
+}
+interface getResponseBookCategory {
+  _embedded: {
+    bookCategory: BookCategory[];
   }
 }
